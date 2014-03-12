8223eea0b13dd321b9965823895df0fff524367e
zhemdaisy <zhemdaisy@gmail.com>
2014-03-12 19:23:11 +0800

    add config

--- view/env.php
+++ view/env.php
@@ -1,3 +1,13 @@
 <?php
 define("PROJECT", dirname(__DIR__));
 require_once PROJECT . 'vendor/autoload.php';
+
+function project_loader($class) {
+    $namespace = "src\\";
+    $length = strlen($namespace);
+    if (substr($class, 0, $length) == $namespace) {
+        require_once PROJECT . $class . '.php';
+    }
+}
+
+spl_autoload_register('project_loader');
