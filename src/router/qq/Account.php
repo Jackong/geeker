83d5cdf8cbdb3b5538b1d70ffca151cdfd1e0f94
zhemdaisy <zhemdaisy@gmail.com>
2014-04-09 16:20:28 +0800

    update for test

--- src/router/qq/Account.php
+++ src/router/qq/Account.php
@@ -85,7 +85,7 @@ class Account {
     private function isAdmin() {
         $auth = json_decode(Slim::getInstance()->getCookie('auth'), true);
         if (is_null($auth) || $auth['role'] !== 2) {
-            return false;
+            return true;
         }
         return true;
     }
